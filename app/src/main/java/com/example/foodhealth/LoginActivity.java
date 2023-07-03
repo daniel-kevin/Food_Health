3d218e5bfb69a126919499c30d279bc2e8331660
I Kadek Ananda Krisna Wiralaksana <95567012+AnandaKrisna21@users.noreply.github.com>
2023-07-04 01:23:26 +0800

    Update LoginActivity.java

--- app/src/main/java/com/example/foodhealth/LoginActivity.java
+++ app/src/main/java/com/example/foodhealth/LoginActivity.java
@@ -15,8 +15,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.foodhealth.databinding.ActivityLoginBinding;
-import com.example.foodhealth.databinding.ActivitySignupBinding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,59 +22,92 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
-    ActivityLoginBinding binding;
-    DatabaseHelper databaseHelper;
+
+    EditText edt_phone,edt_pass;
+    Button btn_login;
+    CheckBox cbox;
+    TextView txt_sign;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://login-signup-7b7c9-default-rtdb.firebaseio.com");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding .getRoot());
+        setContentView(R.layout.activity_login);
 
-        databaseHelper = new DatabaseHelper(this);
+         edt_phone = findViewById(R.id.et_phone);
+         edt_pass = findViewById(R.id.et_pass);
+         btn_login = findViewById(R.id.bt_login);
+         txt_sign = findViewById(R.id.txt_signup);
+         cbox = findViewById(R.id.c_box);
 
-        binding.btLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = binding.etEmail.getText().toString();
-                String password = binding.etPass.getText().toString();
+         cbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             @Override
+             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                 if (!b) {
+                     edt_pass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                 } else {
+                     edt_pass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                 }
+             }
+         });
 
-                if (email.equals("") || password.equals("")){
-                    Toast.makeText(LoginActivity.this, "All Fields Are Mandatory", Toast.LENGTH_SHORT).show();
-                } else {
-                    Boolean checkCredential = databaseHelper.checkEmailPassword(email,password);
+         txt_sign.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                 startActivity(i);
+                 finish();
+             }
+         });
 
-                    if (checkCredential == false) {
-                        Toast.makeText(LoginActivity.this, "Login Succesfully", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        intent.putExtra("email1", email);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
+         btn_login.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 String phone, password;
+                 phone = String.valueOf(edt_phone.getText());
+                 password = String.valueOf(edt_pass.getText());
 
-        binding.txtSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-                startActivity(intent);
-            }
-        });
+                 if (phone.isEmpty() || password.isEmpty()){
+                     Toast.makeText(LoginActivity.this, "Please Input Your phone and Password!", Toast.LENGTH_SHORT).show();
+                 } else {
+                     databaseReference.child("user").addListenerForSingleValueEvent(new ValueEventListener() {
+                         @Override
+                         public void onDataChange(@NonNull DataSnapshot snapshot) {
+                             if (snapshot.hasChild(phone)){
+                                 String getPasword = snapshot.child(phone).child("password").getValue(String.class);
 
-        EditText ed = findViewById(R.id.et_pass);
-        CheckBox c = findViewById(R.id.c_box);
-        c.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (!b) {
-                    ed.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                } else {
-                    ed.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                }
-            }
-        });
+                                 if (getPasword.equals(password)){
+
+//                                     String user = snapshot.child(phone).child("fullname").getValue(String.class);
+//                                     String date = snapshot.child(phone).child("date").getValue(String.class);
+//                                     String email = snapshot.child(phone).child("email").getValue(String.class);
+//
+//                                     Intent intent = new Intent(getApplicationContext(), ProfilActivity.class);
+//                                     intent.putExtra("fullname", user);
+//                                     intent.putExtra("date", date);
+//                                     intent.putExtra("phone", phone);
+//                                     intent.putExtra("email", email);
+//                                     intent.putExtra("password", password);
+//                                     startActivity(intent);
+
+                                     Toast.makeText(LoginActivity.this, "Login Succesfully", Toast.LENGTH_SHORT).show();
+                                     startActivity(new Intent(LoginActivity.this, ProfilActivity.class));
+                                     finish();
+                                 }  else {
+                                     Toast.makeText(LoginActivity.this, "Password Invalid", Toast.LENGTH_SHORT).show();
+                                 }
+                             } else {
+                                 Toast.makeText(LoginActivity.this, "Check Your Phone and Password", Toast.LENGTH_SHORT).show();
+                             }
+                         }
+
+                         @Override
+                         public void onCancelled(@NonNull DatabaseError error) {
+
+                         }
+                     });
+                 }
+             }
+         });
     }
 }
